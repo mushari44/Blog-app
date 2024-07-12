31580734e5a88af6c4dca950b4bc9fe6e4e0eb28
mushari44 <meeshoo-010@hotmail.com>
2024-07-12 05:26:57 +0300

    testing

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    process.env.MONGODB_URL ||
+    process.env.MONGO_DB_URL ||
       "mongodb+srv://musharizh56:admin@cluster0.clvs4os.mongodb.net/blogs"
   )
   .then(() => console.log("Connected mongo db"))
